b8f201e4f78f2e1158d476db5065810dbc8d35f9
septIO <in@sept.io>
2015-08-17 16:55:51 +0200

    Improved filters

--- public/js/app.js
+++ public/js/app.js
@@ -4,33 +4,55 @@ app.controller('mainController', ['$scope', '$http', function ($scope, $http) {
   $scope.selectedGod;
   $scope.settings = {
     magical : true,
-    physical : true
+    physical : true,
+    globalStrats : true,
+    godStrats : true
   }
   
   var settings = $scope.settings;
   
   $http.get('/js/gods.json').then(function(res){
     $scope.gods = res.data;
-  })
+  });
+  $http.get('/js/general.json').then(function(res){
+    $scope.globalStrats = res.data;
+  });
   
   $scope.randomize = function(){
-    var result;
-    var count = 0;
-    for (var prop in $scope.gods){
-        if (Math.random() < 1/++count) result = prop;
-    }
-    var g = $scope.gods[result];
+    var g = $scope.gods[$scope.getRandomKey($scope.gods)];
+    var strat = angular.copy($scope.globalStrats[$scope.getRandomKey($scope.globalStrats)]);
     
-    if(!settings[g.damage]){
+    if(!settings[g.damage] || !$scope.stratIsAllowed(g, strat)){
       $scope.randomize();
       return;
     }
-    console.log(settings[g.damage]);
     
-    $scope.selectedGod = $scope.gods[result];
-    console.log($scope.selectedGod);
+    strat.name = strat.name.replace(/\$godName/g, g.name);
+    $scope.selectedStrat = strat;
+    $scope.selectedGod = g;
   }
   
+  
+  $scope.getRandomKey = function(object){
+    var result;
+    var count = 0;
+    for (var prop in object){
+        if (Math.random() < 1/++count) result = prop;
+    }
+    return result
+  }
+  
+  $scope.stratIsAllowed = function(god, strat){
+    console.log(god);
+    console.log(strat);
+    var allowed = false;  // assert a reroll from the start
+    
+    allowed = strat.allowedRole == 'all' || strat.allowedRole.indexOf(god.role) !== -1;
+    allowed = strat.allowedPantheon == 'all' || strat.allowedPantheon.indexOf(god.pantheon) !== -1;
+    console.log(allowed);
+    
+    return allowed;
+  }
   
 }]);
 
@@ -38,13 +60,6 @@ app.filter('fixName', function(){
   return function(input){
     if(typeof input !== 'undefined')
       return input.replace(/'/g, '').replace(/ /g, '');
-  }
-});
-
-app.filter('uppercaseName', function(){
-  return function(input){
-    if(typeof input !== 'undefined')
-      return input.capitalize();
   }
 });
 
